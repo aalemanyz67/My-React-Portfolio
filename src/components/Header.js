bab766c7d98107e41ca6a6af67a692da7fe910d1
aalemanyz67 <albertaleman67@gmail.com>
2023-12-05 23:44:42 -0600

    updated files in components folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "./Navigation";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container, Row, Col } from "react-bootstrap";
 import "../style/Header.css";
 
